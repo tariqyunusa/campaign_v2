e032248eb5dda41e0d8c5a9181a877dc8d5975ac
Tariq Yunusa <tariikhyunusa@gmail.com>
2024-09-23 23:14:27 +0100

    initail commit

---
+++ src/Data.js
@@ -0,0 +1,64 @@
+export const Slider = [{
+    name: "Misolo Cosmetics", 
+    works: ["Creative Direction", "Art Direction", "Web Presence"],
+    image: "/misolo.webp"
+},
+{
+    name: "Regals Seeds", 
+    works: ["Creative Direction", "Web Presence"],
+    image: "/seeds.webp"
+},
+{
+    name: "Sibaristica Coffee", 
+    works: ["Creative Direction", "Art Direction", "Web Presence"],
+    image: "/sibaristicas.webp"
+},
+{
+    name: "Tag Labs", 
+    works: [ "Web Presence"],
+    image: "/drone.webp"
+}
+]
+
+export const Testimonies = [
+    {
+        org: " Urban Leaf Design",
+        quote: "The Campaign team took our ideas and turned them into a fully interactive experience. The new website has been praised for its design and usability, and our clients love how easy it is to navigate. We highly recommend them for anyone looking for top-tier digital solutions.",
+        person: "– Founder, Urban Leaf Design",
+        color: "#343A1A"
+    },
+    {
+        org: "Horizon Health",
+        quote: "Campaign brought our vision to life with a website that not only looks incredible but functions flawlessly. Their team’s creativity and technical expertise were evident throughout the project, and we’ve seen a noticeable increase in user engagement since the relaunch",
+        person: "– CEO, Horizon Health",
+        color: "#392A16"
+    },
+    {
+        org: "Blue Sky Ventures",
+        quote: "Working with Campaign was a game changer for us. They transformed our outdated website into a modern, interactive platform that truly represents our brand. Their attention to detail and seamless communication made the process smooth from start to finish.",
+        person: "– Marketing Director, Blue Sky Ventures",
+        color: "#474448"
+    },
+    {
+        org: "Elevate Consulting",
+        quote: "Campaign’s ability to blend aesthetics with functionality is unmatched. They created a sleek and professional website for us that’s easy to manage and update. Their understanding of modern web design trends helped us stand out in a competitive market.",
+        person: "– Operations Manager, Elevate Consulting",
+        color: "#5F7367"
+    }
+]
+export const Hire = [
+    {
+      colors: ["#731DD8", "#FC5130", "#2DC7FF", "#E4FF1A", "#EF2D56"]
+    },
+    {
+      text: [
+        "Checkout our Socials",
+        "Hire the coolest kids on the block.",
+        "Made with love from Lagos, Nigeria.",
+        "Available for work.",
+        "Doing what we do and having a blast.",
+        "We think Kendrick Lamar is cool."
+      ]
+    }
+  ];
+export const LinkColors = ["#B1CF5F", "#E09F3E", "#9E2A2B", "#73E2A7", "#335C67"]
