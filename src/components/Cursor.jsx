1a7a5677988c807e33f7fec5e1bb37a451cb1a18
Tariq Yunusa <tariikhyunusa@gmail.com>
2024-10-04 12:18:20 +0100

    icons instead of image'cursor'

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "../styles/Cursor.css"
+import { FiArrowRight } from "react-icons/fi";
+import { FiArrowLeft } from "react-icons/fi";
 import arrowLeft from "../assets/arrow-left.png"
 import arrowRight from "../assets/arrow-right.png"
 
@@ -8,10 +10,12 @@ const Cursor = ({x,y, visible, arrowDirection}) => {
     <div className={`aux__cursor`}  style={{ left: `${x}px`, top: `${y}px`, opacity: visible ? 1 : 0 }}>
      <div className="direction_wrapper__cursor">
      {arrowDirection === 'left' && (
-        <img src={arrowLeft} alt="arrow-left" />
+        // <img src={arrowLeft} alt="arrow-left" />
+        <FiArrowLeft />
       )}
       {arrowDirection === 'right' && (
-        <img src={arrowRight} alt="arrow-right" />
+        // <img src={arrowRight} alt="arrow-right" />
+        <FiArrowRight />
       )}
      </div>
     </div>
