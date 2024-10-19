d8a13be2f2de7c03d01858e94d04928340dff5b7
Tariq Yunusa <tariikhyunusa@gmail.com>
2024-10-19 20:34:58 +0100

    added basic ul layout

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,6 +16,26 @@ const Nav = ({color}) => {
         <img src={color.logo} alt="Logo"  />
       </div>
       <div className='nav__filler'></div>
+      <ul className="nav__ul ">
+        <div className='ul__nav_upper'>
+        <div className ='links__wrapper'>
+          <li><a href="" className='nav__links__item'>Home</a></li>
+          <li><a href="" className='nav__links__item'>About</a></li>
+          <li><a href="" className='nav__links__item'>Works</a></li>
+          <li><a href="" className='nav__links__item'>Contact</a></li>
+          <li><a href="" className='nav__links__item'>Hire Me</a></li>
+        </div>
+        <div className="img__nav__wrapper">
+          <img src="" alt=""  />
+        </div>
+        </div>
+        <div className="nav__links_fillers">
+          <p>Nav design concept by studio lumio</p>
+          <p>Project built by Tariq Yunusa</p>
+          <p><span className='nav__links_filler_span'>Font: </span>Suse</p>
+          
+        </div>
+      </ul>
     </nav>
   )
 }
