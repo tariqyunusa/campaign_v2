e13ffc0a6bdfd7bd7f31fab0a5d8df4e3cb9fd03
Tariq Yunusa <tariikhyunusa@gmail.com>
2024-10-27 16:26:12 +0100

    added functinlity on nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,66 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Nav.css';
-import { FiMenu } from "react-icons/fi";
+import { FiMenu, FiX } from "react-icons/fi";
 import Logo from '../assets/Subtract.svg';
 import { links } from '../Data';
-import { FiX } from "react-icons/fi";
 
 const Nav = ({ color }) => {
-  // State to track which link is hovered
-  const [hoveredLinkIndex, setHoveredLinkIndex] = useState(null);
+  const [hoveredLinkIndex, setHoveredLinkIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false)
+
+  // Preload images on component mount
+  useEffect(() => {
+    links.forEach(link => {
+      const img = new Image();
+      img.src = link.image;
+    });
+  }, []);
 
   const handleMouseEnter = (index) => {
-    // Only update state if the hovered link is not already the current one
-    if (hoveredLinkIndex !== index) {
-      setHoveredLinkIndex(index);
-    }
+    setHoveredLinkIndex(index);
   };
 
-  const handleMouseLeave = () => {
-    // Only clear state if it's not already null
-    if (hoveredLinkIndex !== null) {
-      setHoveredLinkIndex(null);
-    }
-  };
+  const toggleNav = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <nav className='nav_container'>
-      <div className='nav__menu'>
+      <div className='nav__menu' onClick={toggleNav}>
         <FiMenu className='menu' style={{ color: color.menu }} />
       </div>
       <div className='nav__logo'>
         <img src={color.logo} alt="Logo" />
       </div>
       <div className='nav__filler'></div>
-      <ul className="nav__ul">
+      <ul className={`nav__ul ${isOpen ? "nav__open" : ""}`}>
         <div className='ul__nav_upper'>
           <div className='links__wrapper'>
             {links.map((link, i) => (
               <li
                 key={i}
-                onMouseEnter={() => handleMouseEnter(i)} // Call handler on hover
-                onMouseLeave={handleMouseLeave} // Call handler on mouse leave
+                onMouseEnter={() => handleMouseEnter(i)}
+                // onMouseLeave={handleMouseLeave} 
               >
                 <a href={link.path} className='nav__links__item'>{link.name}</a>
               </li>
             ))}
           </div>
-          <div className="img__nav__wrapper">
-            {hoveredLinkIndex !== null && links[hoveredLinkIndex] && (
-              <img
-                src={links[hoveredLinkIndex].image}
-                alt={links[hoveredLinkIndex].name}
-              />
-            )}
-          </div>
+          {hoveredLinkIndex !== null && (
+            <div
+              className="img__nav__wrapper"
+              style={{ backgroundImage: `url(${links[hoveredLinkIndex].image})` }}
+            ></div>
+          )}
         </div>
         <div className="nav__links_fillers">
           <div className='nav__links_span_wrapper'>
             <p><span className='nav__links_filler_span'>Nav design concept by</span> studio lumio</p>
             <p><span className='nav__links_filler_span'>Project built by</span> Tariq Yunusa</p>
-            <p><span className='nav__links_filler_span'>Font: </span>Suse</p>
+            <p><span className='nav__links_filler_span'>Font:</span> Suse</p>
           </div>
-          <div className='icon__close'><FiX /></div>
+          <div className='icon__close' onClick={toggleNav}><FiX /></div>
         </div>
       </ul>
     </nav>
